6f0ec10c557ebfeafe0b6f2b042e620b351622c5
Metod Medja <metod@medja.net>
2014-10-12 01:30:31 +0200

    options file rework and new options

--- Lua/Options.lua
+++ Lua/Options.lua
@@ -1,8 +1,19 @@
-_G.TeamSpeak.Options = {
-	Colors = {
-		Global = Color("FFD400"),
-		Channel = Color("FFFFFF")
-	},
-	FixChatLag = true,
-	ChatHistory = 20
-}
+function TS.Options:init()
+	-- Colors used for TeamSpeak messages
+	self.colors = {
+		info    = Color("FFD400"),
+		global  = Color("33CCFF"),
+		channel = Color("FFFFFF"),
+		private = Color("828282")}
+
+	-- Enables autocomplete in chat
+	self.autocomplete = true
+	-- Removes duplicate messages caused by lag
+	self.fix_chat_lag = true
+	-- Maximum lenght of chat history
+	self.chat_history = 20
+	-- Clears the chat input when esceape is pressed
+	self.clear_input_on_escape_key = false
+	-- Keeps chat input between screens
+	self.restore_chat_input = true
+end
